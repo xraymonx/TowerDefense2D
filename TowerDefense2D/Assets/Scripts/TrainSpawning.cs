64e71390740a8896296f3cbd5f4b515bc3479b89
xraymonx <raymon_jesse2@hotmail.com>
2015-11-24 19:29:28 +0100

    Train goes forward with the right rotation! Took me a good 5 hours but worth the time. Choo Choo!

---
+++ TowerDefense2D/Assets/Scripts/TrainSpawning.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainSpawning : MonoBehaviour {
+	[SerializeField] GameObject _train;
+	public float counter = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += (1 * Time.deltaTime); 
+		if (counter >= 5f) 
+		{
+			Instantiate(_train, this.transform.position, this.transform.rotation);
+			counter = 0;
+			Debug.Log(counter);
+		}
+	}
+}
